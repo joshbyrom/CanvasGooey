cc5bf88cd35a566926a63c335a0e65dfebadf693
joshbyrom <joshbyrom@gmail.com>
2014-02-24 13:58:02 -0500

    childByName getter and monitoring variables

--- src/GUIElement.js
+++ src/GUIElement.js
@@ -1,9 +1,32 @@
-var GUIElement = function(parent, pubSub) {
+var GUIElement = function(parent, pubSub, name) {
+    this.name = name;
+
+    this.active = true;
+    this.visible = true;
+
+    // all variables prefixed with *last* are monitoring variables
+    // and should not be changed manually
+    this.lastActive = this.active;
+    this.lastVisible = this.visible;
+
+    this.selected = false;
+    this.mouseOver = false;
+
+    this.lastSelected = this.selected;
+    this.lastMouseOver = this.mouseOver;
+
     this.x = 0;
     this.y = 0;
 
+    this.lastX = this.x;
+    this.lastY = this.y;
+
     this.width = 0;
     this.height = 0;
+
+    this.lastWidth = this.width;
+    this.lastHeight = this.height;
+
     this.pubSub = pubSub;
 
     this.parent = parent;
@@ -11,12 +34,20 @@ var GUIElement = function(parent, pubSub) {
 };
 
 GUIElement.prototype.render = function() {
+    if(!active) return;
+
+    if(!visible) {
+        // TODO render grayscale?
+        return;
+    }
 
     this.pubSub.emit("Render");
 };
 
 GUIElement.prototype.update = function() {
+    if(!active) return;
 
+    this.monitorState();
     this.pubSub.emit("Update");
 };
 
@@ -55,6 +86,20 @@ GUIElement.prototype.removeChild = function(child) {
 
 GUIElement.prototype.hasChild = function(child) {
     return this.children.indexOf(child) >= 0;
+};
+
+GUIElement.prototype.getChildByName = function(name) {
+    for(var i = 0; i < this.children.length; ++i) {
+        if(this.children[i].name === name) {
+            return this.children[i];
+        }
+    }
+
+    return undefined;
+};
+
+GUIElement.property.monitorState = function() {
+
 };
 
 var createGUIElement = function() {
